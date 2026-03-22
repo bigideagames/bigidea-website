90926ca356f78b0e2ebabebb5309b71d11efa658
Travis Brown-John <travis@bigidea.games>
2026-03-21 20:20:35 -0700

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/bigidea-website/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
